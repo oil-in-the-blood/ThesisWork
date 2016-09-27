3d6db484aa3022e454fc76274192edb5e8e4b217
oil-in-the-blood <Dan@Daniels-MacBook-Air.local>
2016-09-27 08:58:59 -0400

    New Run Really Added

---
+++ Matlab/RUN_092716.m
@@ -0,0 +1,14 @@
+%The Run is to gather the RTM for the CSK-TS modulus system
+
+fSave = 'CSK_TS_RUN_092716.txt';
+SetupConstants;
+CSK_TS_MOD_PulseTrain                                  %Opens Model
+set_param('CSK_TS_MOD_PulseTrain','StopTime','800');   %Sets Stop Time
+sim('CSK_TS_MOD_PulseTrain');                          %Runs Simulation
+
+[a1,a0,b1,b0,max,min] = RTM(x1,m);                     %Get Return Map Attack
+
+pdata = ProcessSync(t,sync,freq,weight,cut);
+[allbits,misses] = ErrChk(t,pdata,m,freq);
+
+savefileforRTM;
